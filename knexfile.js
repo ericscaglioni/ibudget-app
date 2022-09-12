8615a9e420e1ca94d0b107edd4d66f62330f4e4d
ericscaglioni <ericsf34@gmail.com>
2022-09-12 17:34:05 +0200

    chore: add first migration: users

---
+++ knexfile.js
@@ -0,0 +1,47 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+ module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: "ibudgetdb",
+      user: "postgres",
+      password: "teste",
+    },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "<Your Staging DB>",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "<Your Production DB>",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
